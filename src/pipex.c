9d086746f236fbf21fb081c56f63deb70c63c65d
wnocchi <wnocchi@student.42.fr>
2024-02-24 16:57:11 +0100

    need norminette

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:24:28 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/02/24 16:11:16 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/02/24 16:20:50 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ char	*join_path_access(char *av, char **envp)
 	return (NULL);
 }
 
-void	free_cmd_path(t_pipex *pipex)
+void	free_close(t_pipex *pipex)
 {
 	if (pipex->cmd1)
 		free_tabs(pipex->cmd1);
@@ -76,6 +76,10 @@ void	free_cmd_path(t_pipex *pipex)
 		free(pipex->path1);
 	if (pipex->path2)
 		free(pipex->path2);
+	close(pipex->in);
+	close(pipex->out);
+	close(pipex->pipein[0]);
+	close(pipex->pipein[1]);
 }
 
 // int	exec_child1(pid_t child1, int *pipein[2], )
@@ -121,7 +125,7 @@ int	creat_child1(t_pipex *pipex, char **envp)
 		close(pipex->in);
 		 if (execve(pipex->path1, pipex->cmd1, envp) == -1)
 		 	perror("");
-		free_cmd_path(pipex);
+		free_close(pipex);
 		exit(0);
 	}
 	return(0);
@@ -142,7 +146,7 @@ int	creat_child2(t_pipex *pipex, char **envp)
 		close(pipex->out);
 		if (execve(pipex->path2, pipex->cmd2, envp) == -1)
             perror("");
-		free_cmd_path(pipex);
+		free_close(pipex);
 		exit(0);
 	}
 	return (0);
@@ -150,7 +154,6 @@ int	creat_child2(t_pipex *pipex, char **envp)
 
 int main(int ac, char **av, char **envp)
 {
-	(void)ac;
 	t_pipex pipex;
 
 	if(ac != 5)
@@ -161,51 +164,15 @@ int main(int ac, char **av, char **envp)
 	pipex.out = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if(pipex.out == -1)
 		return(perror(av[4]), 1);
+	pipe(pipex.pipein);
 	pipex.cmd1 = ft_split(av[2], ' ');
 	pipex.cmd2 = ft_split(av[3], ' ');
-	pipe(pipex.pipein);
 	pipex.path1 = join_path_access(pipex.cmd1[0], envp);
 	pipex.path2 = join_path_access(pipex.cmd2[0], envp);
 	pipex.in = open(av[1], O_RDONLY);
 	creat_child1(&pipex, envp);
 	creat_child2(&pipex, envp);
-	// pipex.pid1 = fork();
-	// if(pipex.pid1 == -1)
-	// 	perror("");
-	// if(pipex.pid1 == 0)
-	// {
-	// 	close(pipex.out);
-	// 	close(pipex.pipein[0]);
-	// 	dup2(pipex.in, STDIN_FILENO);
-	// 	dup2(pipex.pipein[1], STDOUT_FILENO);
-	// 	close(pipex.pipein[1]);
-	// 	close(pipex.in);
-	// 	 if (execve(pipex.path1, pipex.cmd1, envp) == -1)
-	// 	 	perror("");
-	// 	free_cmd_path(&pipex);
-	// 	exit(0);
-	// }
-	// pipex.pid2 = fork();
-	// if (pipex.pid2 == -1)
-	// 	return (perror(""), 1);
-	// if (pipex.pid2 == 0)
-	// {
-	// 	close(pipex.in);
-	// 	close(pipex.pipein[1]);
-	// 	dup2(pipex.pipein[0], STDIN_FILENO);
-	// 	dup2(pipex.out, STDOUT_FILENO);
-	// 	close(pipex.pipein[0]);
-	// 	close(pipex.out);
-	// 	if (execve(pipex.path2, pipex.cmd2, envp) == -1)
-    //         perror("");
-	// 	free_cmd_path(&pipex);
-	// 	exit(0);
-	// }
-	close(pipex.in);
-	close(pipex.out);
-	close(pipex.pipein[0]);
-	close(pipex.pipein[1]);
-	free_cmd_path(&pipex);
+	free_close(&pipex);
 	waitpid(pipex.pid1, NULL, 0);
 	waitpid(pipex.pid2, NULL, 0);
 	return(0);
